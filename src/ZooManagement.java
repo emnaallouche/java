a86066fabc81e0ceb05dd9d1d75028750f167b34
emnaallouche <emna.allouche@esprit.tn>
2024-09-29 11:25:22 +0100

    Prosit 2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 
 class ZooManagement {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int nbrCages ;
-        String zooName ;
-        System.out.printf("Entrez le nom du zoo ");
-        zooName = scanner.nextLine();
-        System.out.print("Entrez le nombre de cages : ");
-        nbrCages = scanner.nextInt();
-        System.out.printf("%s comporte %d cages%n", zooName, nbrCages);
-        scanner.close();
+        Animal lion = new Animal("family","lionn",30,true);
+        Zoo myZoo = new Zoo("belvidaire", "tunis",25);
+        lion.displayAnimal();
+        myZoo.displayZoo();
+        myZoo.animals[0] = lion;
+        System.out.println(myZoo);
     }
 }
